d358abe00f585732ba07a4aaf3335b246ab7bbdd
Yury <u44788200@gmail.com>
2020-09-28 21:30:04 +0300

    upd blog

--- app/Http/Controllers/BlogController.php
+++ app/Http/Controllers/BlogController.php
@@ -2,11 +2,17 @@
 
 namespace App\Http\Controllers;
 
+use App\BlogPage;
 use Illuminate\Http\Request;
 
 class BlogController extends Controller
 {
     public function index() {
-        return view('blog');
+
+        $cards = BlogPage::paginate(20);
+
+        return view('blog', [
+            'cards' => $cards,
+        ]);
     }
 }
--- resources/views/modules/blog/cards.blade.php
+++ resources/views/modules/blog/cards.blade.php
@@ -2,150 +2,18 @@
     <div class="container">
         <h2 class="section-title">Блог</h2>
         <div class="discount-wrapper row">
+            @foreach($cards as $card)
             <div class="col-lg-3 col-md-4 col-sm-6 col-6">
-                <div class="card mb-4"><a class="card__link" href=""><img class="card__image lazy" data-src="../img/cars/1.jpg" alt="..."/></a>
+                <div class="card mb-4"><a class="card__link" href=""><img class="card__image lazy" data-src="{{ asset('storage/' . $card->image) }}" alt="..."/></a>
                     <div class="card__body">
-                        <p class="card__date">01.08.2020</p>
-                        <p class="card__text">Lorem ipsum dolor sit amet.</p>
-                        <p class="card__text">Some quick example text to build on the card title and make up the bulk of the card's content.</p></p><a class="button button--second card__button" href="">Подробнее</a>
+                        <p class="card__date">{{ (new DateTimeImmutable($card->created_at))->format('d.m.Y') }}</p>
+                        <p class="card__text">{{ $card->title }}</p>
+                        <p class="card__text">{!! mb_substr($card->text, 0, 150) . '...' !!}</p></p><a class="button button--second card__button" href="{{ "/blog/$card->id" }}">Подробнее</a>
                     </div>
                 </div>
             </div>
-            <div class="col-lg-3 col-md-4 col-sm-6 col-6">
-                <div class="card mb-4"><a class="card__link" href=""><img class="card__image lazy" data-src="../img/cars/2.jpg" alt="..."/></a>
-                    <div class="card__body">
-                        <p class="card__date">02.08.2020</p>
-                        <p class="card__text">Lorem ipsum dolor sit amet.</p>
-                        <p class="card__text">Some quick example text to build on the card title and make up the bulk of the card's content.</p></p><a class="button button--second card__button" href="">Подробнее</a>
-                    </div>
-                </div>
-            </div>
-            <div class="col-lg-3 col-md-4 col-sm-6 col-6">
-                <div class="card mb-4"><a class="card__link" href=""><img class="card__image lazy" data-src="../img/cars/3.jpg" alt="..."/></a>
-                    <div class="card__body">
-                        <p class="card__date">03.08.2020</p>
-                        <p class="card__text">Lorem ipsum dolor sit amet.</p>
-                        <p class="card__text">Some quick example text to build on the card title and make up the bulk of the card's content.</p></p><a class="button button--second card__button" href="">Подробнее</a>
-                    </div>
-                </div>
-            </div>
-            <div class="col-lg-3 col-md-4 col-sm-6 col-6">
-                <div class="card mb-4"><a class="card__link" href=""><img class="card__image lazy" data-src="../img/cars/4.jpg" alt="..."/></a>
-                    <div class="card__body">
-                        <p class="card__date">04.08.2020</p>
-                        <p class="card__text">Lorem ipsum dolor sit amet.</p>
-                        <p class="card__text">Some quick example text to build on the card title and make up the bulk of the card's content.</p></p><a class="button button--second card__button" href="">Подробнее</a>
-                    </div>
-                </div>
-            </div>
-            <div class="col-lg-3 col-md-4 col-sm-6 col-6">
-                <div class="card mb-4"><a class="card__link" href=""><img class="card__image lazy" data-src="../img/cars/5.jpg" alt="..."/></a>
-                    <div class="card__body">
-                        <p class="card__date">05.08.2020</p>
-                        <p class="card__text">Lorem ipsum dolor sit amet.</p>
-                        <p class="card__text">Some quick example text to build on the card title and make up the bulk of the card's content.</p></p><a class="button button--second card__button" href="">Подробнее</a>
-                    </div>
-                </div>
-            </div>
-            <div class="col-lg-3 col-md-4 col-sm-6 col-6">
-                <div class="card mb-4"><a class="card__link" href=""><img class="card__image lazy" data-src="../img/cars/6.jpg" alt="..."/></a>
-                    <div class="card__body">
-                        <p class="card__date">06.08.2020</p>
-                        <p class="card__text">Lorem ipsum dolor sit amet.</p>
-                        <p class="card__text">Some quick example text to build on the card title and make up the bulk of the card's content.</p></p><a class="button button--second card__button" href="">Подробнее</a>
-                    </div>
-                </div>
-            </div>
-            <div class="col-lg-3 col-md-4 col-sm-6 col-6">
-                <div class="card mb-4"><a class="card__link" href=""><img class="card__image lazy" data-src="../img/cars/1.jpg" alt="..."/></a>
-                    <div class="card__body">
-                        <p class="card__date">01.08.2020</p>
-                        <p class="card__text">Lorem ipsum dolor sit amet.</p>
-                        <p class="card__text">Some quick example text to build on the card title and make up the bulk of the card's content.</p></p><a class="button button--second card__button" href="">Подробнее</a>
-                    </div>
-                </div>
-            </div>
-            <div class="col-lg-3 col-md-4 col-sm-6 col-6">
-                <div class="card mb-4"><a class="card__link" href=""><img class="card__image lazy" data-src="../img/cars/2.jpg" alt="..."/></a>
-                    <div class="card__body">
-                        <p class="card__date">02.08.2020</p>
-                        <p class="card__text">Lorem ipsum dolor sit amet.</p>
-                        <p class="card__text">Some quick example text to build on the card title and make up the bulk of the card's content.</p></p><a class="button button--second card__button" href="">Подробнее</a>
-                    </div>
-                </div>
-            </div>
-            <div class="col-lg-3 col-md-4 col-sm-6 col-6">
-                <div class="card mb-4"><a class="card__link" href=""><img class="card__image lazy" data-src="../img/cars/3.jpg" alt="..."/></a>
-                    <div class="card__body">
-                        <p class="card__date">03.08.2020</p>
-                        <p class="card__text">Lorem ipsum dolor sit amet.</p>
-                        <p class="card__text">Some quick example text to build on the card title and make up the bulk of the card's content.</p></p><a class="button button--second card__button" href="">Подробнее</a>
-                    </div>
-                </div>
-            </div>
-            <div class="col-lg-3 col-md-4 col-sm-6 col-6">
-                <div class="card mb-4"><a class="card__link" href=""><img class="card__image lazy" data-src="../img/cars/4.jpg" alt="..."/></a>
-                    <div class="card__body">
-                        <p class="card__date">04.08.2020</p>
-                        <p class="card__text">Lorem ipsum dolor sit amet.</p>
-                        <p class="card__text">Some quick example text to build on the card title and make up the bulk of the card's content.</p></p><a class="button button--second card__button" href="">Подробнее</a>
-                    </div>
-                </div>
-            </div>
-            <div class="col-lg-3 col-md-4 col-sm-6 col-6">
-                <div class="card mb-4"><a class="card__link" href=""><img class="card__image lazy" data-src="../img/cars/5.jpg" alt="..."/></a>
-                    <div class="card__body">
-                        <p class="card__date">05.08.2020</p>
-                        <p class="card__text">Lorem ipsum dolor sit amet.</p>
-                        <p class="card__text">Some quick example text to build on the card title and make up the bulk of the card's content.</p></p><a class="button button--second card__button" href="">Подробнее</a>
-                    </div>
-                </div>
-            </div>
-            <div class="col-lg-3 col-md-4 col-sm-6 col-6">
-                <div class="card mb-4"><a class="card__link" href=""><img class="card__image lazy" data-src="../img/cars/6.jpg" alt="..."/></a>
-                    <div class="card__body">
-                        <p class="card__date">06.08.2020</p>
-                        <p class="card__text">Lorem ipsum dolor sit amet.</p>
-                        <p class="card__text">Some quick example text to build on the card title and make up the bulk of the card's content.</p></p><a class="button button--second card__button" href="">Подробнее</a>
-                    </div>
-                </div>
-            </div>
-            <div class="col-lg-3 col-md-4 col-sm-6 col-6">
-                <div class="card mb-4"><a class="card__link" href=""><img class="card__image lazy" data-src="../img/cars/1.jpg" alt="..."/></a>
-                    <div class="card__body">
-                        <p class="card__date">01.08.2020</p>
-                        <p class="card__text">Lorem ipsum dolor sit amet.</p>
-                        <p class="card__text">Some quick example text to build on the card title and make up the bulk of the card's content.</p></p><a class="button button--second card__button" href="">Подробнее</a>
-                    </div>
-                </div>
-            </div>
-            <div class="col-lg-3 col-md-4 col-sm-6 col-6">
-                <div class="card mb-4"><a class="card__link" href=""><img class="card__image lazy" data-src="../img/cars/2.jpg" alt="..."/></a>
-                    <div class="card__body">
-                        <p class="card__date">02.08.2020</p>
-                        <p class="card__text">Lorem ipsum dolor sit amet.</p>
-                        <p class="card__text">Some quick example text to build on the card title and make up the bulk of the card's content.</p></p><a class="button button--second card__button" href="">Подробнее</a>
-                    </div>
-                </div>
-            </div>
-            <div class="col-lg-3 col-md-4 col-sm-6 col-6">
-                <div class="card mb-4"><a class="card__link" href=""><img class="card__image lazy" data-src="../img/cars/3.jpg" alt="..."/></a>
-                    <div class="card__body">
-                        <p class="card__date">03.08.2020</p>
-                        <p class="card__text">Lorem ipsum dolor sit amet.</p>
-                        <p class="card__text">Some quick example text to build on the card title and make up the bulk of the card's content.</p></p><a class="button button--second card__button" href="">Подробнее</a>
-                    </div>
-                </div>
-            </div>
-            <div class="col-lg-3 col-md-4 col-sm-6 col-6">
-                <div class="card mb-4"><a class="card__link" href=""><img class="card__image lazy" data-src="../img/cars/4.jpg" alt="..."/></a>
-                    <div class="card__body">
-                        <p class="card__date">04.08.2020</p>
-                        <p class="card__text">Lorem ipsum dolor sit amet.</p>
-                        <p class="card__text">Some quick example text to build on the card title and make up the bulk of the card's content.</p></p><a class="button button--second card__button" href="">Подробнее</a>
-                    </div>
-                </div>
-            </div>
+            @endforeach
         </div>
+        {{ $cards->links() }}
     </div>
 </section>
