2cf2d57529711d8cd03f01b9f12cecd408b98ff6
Yury <u44788200@gmail.com>
2020-11-13 18:36:20 +0300

    auction inner-text

--- resources/views/auction.blade.php
+++ resources/views/auction.blade.php
@@ -6,11 +6,13 @@
 
     <section class="section">
         <div class="container">
-            <h2 class="section-title">{{ $auction->title }}</h2>
-            @if($auction->image)
-            <img class="image image--full" src="{{ asset("storage/$auction->image")  }}" alt="{{ $auction->title }}"/>
-            @endif
-            {!! $auction->text !!}
+            <div class="inner-text">
+                <h2 class="section-title">{{ $auction->title }}</h2>
+                @if($auction->image)
+                    <img class="image image--full" src="{{ asset("storage/$auction->image")  }}" alt="{{ $auction->title }}"/>
+                @endif
+                {!! $auction->text !!}
+            </div>
         </div>
     </section>
 
