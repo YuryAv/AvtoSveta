8cceb1289347046df9344dd8fb02d7cee830285a
Yury <u44788200@gmail.com>
2020-11-13 18:43:09 +0300

    auction image short

--- resources/views/auction.blade.php
+++ resources/views/auction.blade.php
@@ -9,7 +9,7 @@
             <div class="inner-text">
                 <h2 class="section-title">{{ $auction->title }}</h2>
                 @if($auction->image)
-                    <img class="image image--full" src="{{ asset("storage/$auction->image")  }}" alt="{{ $auction->title }}"/>
+                    <img class="image image--short" src="{{ asset("storage/$auction->image")  }}" alt="{{ $auction->title }}"/>
                 @endif
                 {!! $auction->text !!}
             </div>
