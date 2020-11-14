9a12e744a92700aeae669699b9913b764bb802d8
Yury <u44788200@gmail.com>
2020-11-14 22:51:05 +0300

    fix include head-style.css

--- resources/views/modules/style.blade.php
+++ resources/views/modules/style.blade.php
@@ -1,3 +1,3 @@
 <style>
-{{ file_get_contents(asset('css/head-style.css')) }}
+{{ file_get_contents(asset('css/head-style.css'), false, stream_context_create(["ssl"=>array("verify_peer"=>false, "verify_peer_name"=>false)])) }}
 </style>
